fe8c4f4c9df492ba59064e0f7e7722df2c7d1010
robex99 <roberthy81@hotmail.com>
2021-02-13 18:18:10 -0300

    FOrmatação com o prettier

--- game-shop/src/routers/Router.js
+++ game-shop/src/routers/Router.js
@@ -1,66 +1,65 @@
-import React, {useState, useEffect} from 'react'
-import { Products, Navbar, Cart } from '../components';
-import { commerce } from '../lib/commerce';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Products, Navbar, Cart } from "../components";
+import { commerce } from "../lib/commerce";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 export default function Router() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState({});
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
 
-    const fetchProducts = async () => {
-        const { data } = await commerce.products.list();
-        setProducts(data);
-    }
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
 
-    const fetchCart = async () => {
-        setCart(await commerce.cart.retrieve())
-    }
+  const handleAddToCart = async (productId, quantity) => {
+    const { cart } = await commerce.cart.add(productId, quantity);
+    setCart(cart);
+  };
 
-    const handleAddToCart = async (productId, quantity) => {
-        const { cart } = await commerce.cart.add(productId, quantity)
-        setCart(cart)
-    };
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
 
-    const handleUpdateCartQty = async (productId, quantity) => {
-        const { cart } = await commerce.cart.update(productId, { quantity })
+    setCart(cart);
+  };
 
-        setCart(cart)
-    }
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
 
-    const handleRemoveFromCart = async (productId) => {
-        const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
+  };
 
-        setCart(cart);
-    }
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
+  };
 
-    const handleEmptyCart = async () => {
-        const { cart } = await commerce.cart.empty();
-        setCart(cart);
-    }
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
 
-    useEffect(() => {
-        fetchProducts();
-        fetchCart();
-    }, [])
-
-    return (
-        <BrowserRouter>
-            <Navbar totalItems={cart.total_items}/>
-            <Switch>
-                <Route exact path="/">
-                    <Products products={products} onAddToCart={handleAddToCart} />
-                </Route>
-                <Route path="/cart">
-                    <Cart 
-                    cart={cart}
-                    handleUpdateCartQty={handleUpdateCartQty}
-                    handleRemoveFromCart={handleRemoveFromCart}
-                    handleEmptyCart={handleEmptyCart}
-                    totalItems={cart.total_items}
-                    />
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Navbar totalItems={cart.total_items} />
+      <Switch>
+        <Route exact path="/">
+          <Products products={products} onAddToCart={handleAddToCart} />
+        </Route>
+        <Route path="/cart">
+          <Cart
+            cart={cart}
+            handleUpdateCartQty={handleUpdateCartQty}
+            handleRemoveFromCart={handleRemoveFromCart}
+            handleEmptyCart={handleEmptyCart}
+            totalItems={cart.total_items}
+          />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
